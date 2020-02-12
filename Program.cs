96f637a837d87a420e1c25e0622864b2e6c58743
Josh <jvandenbrink1782@gmail.com>
2020-02-12 16:00:52 -0700

    Start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Class_Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
